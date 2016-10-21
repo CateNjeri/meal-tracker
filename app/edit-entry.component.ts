986bcb98c9cfdfdd6fd3c6b1a26d885c262a9560
Tim <tim.bourgault@gmail.com>
2016-10-21 15:02:04 -0700

    add form to edit-entry.component and update its export class.

--- app/edit-entry.component.ts
+++ app/edit-entry.component.ts
@@ -4,9 +4,29 @@ import { Entry } from './entry.model';
 @Component({
   selector: 'edit-entry',
   template: `
+    <div *ngIf="childSelectedEntry">
+      <h1>Edit Entry</h1>
+      <div>
+        <label>Edit Meal Name:</label>
+        <input [(ngModel)]="childSelectedEntry.mealName">
+        <label>Edit Meal Details:</label>
+        <input [(ngModel)]="childSelectedEntry.details">
+      </div>
+      <div>
+        <label>Edit Meal Calories:</label>
+        <input [(ngModel)]="childSelectedEntry.calories">
+        <label>Edit Meal Id Number:</label>
+        <input [(ngModel)]="childSelectedEntry.id">
+        <button (click)="doneClicked()">Submit Edits</button>
+      </div>
+    </div>
   `
 })
 
 export class EditEntryComponent {
-  
+  @Input() childSelectedEntry: Entry;
+  @Output() doneClickedSender = new EventEmitter();
+  doneClicked() {
+    this.doneClickedSender.emit();
+  }
 }
