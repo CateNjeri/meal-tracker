afdfb5b64c699679f92dd90672cf7d51470ebab1
Tim <tim.bourgault@gamil.com>
2016-10-21 11:22:30 -0700

    work out some errors in gulp build.

--- app/calorie-level.pipe.ts
+++ app/calorie-level.pipe.ts
@@ -2,6 +2,10 @@ import {Pipe, PipeTransform} from '@angular/core';
 import {Entry} from './entry.model';
 
 @Pipe({
-  name: "calorie-level"
+  name: "calorie-level",
   pure: false
 })
+
+  export class CalorieLevelPipe implements PipeTransform {
+  
+  }
